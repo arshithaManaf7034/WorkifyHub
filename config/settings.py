c6bd4196e4870d6a626e647697a90fe1d5fb463d
arshithaManaf7034 <arshithamanaf15@gmail.com>
2026-05-23 16:19:35 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -113,17 +113,31 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 LOGIN_URL = '/login/'
-STATIC_URL = 'static/'
-TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
 LOGOUT_REDIRECT_URL = '/'
+
+TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
+
+
+# Static files
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+# Media files
+
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
